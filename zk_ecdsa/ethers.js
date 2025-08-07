0d1d5335b62bd03093a7718b5e078fe657099c2d
PinaComa <pnarcomak@gmail.com>
2025-08-07 16:16:47 +0200

    Clean up GitHub: remove duplicate and misnamed folders

--- zk_ecdsa/ethers.js
+++
@@ -1,7 +0,0 @@
-import { Wallet } from "ethers";
-
-const privateKey = ""; // from keystore or export
-const wallet = new Wallet(privateKey);
-
-const publicKey = wallet.publicKey;  // full uncompressed pubkey (130 hex chars, 0x04 + X + Y)
-console.log(publicKey);
